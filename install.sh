6cff833197cb1793d9dff0f92be2624ade3724cc
rozicdejan <drozic1989@gmail.com>
2024-05-26 15:28:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 sudo docker volume create linkstack
-sudo docker compose up -d
+sudo docker run --detach \
+    --name linkstack \
+    --publish 280:80 \
+    --publish 2443:443 \
+    --restart unless-stopped \
+    --mount source=linkstack,target=/htdocs \
+    linkstackorg/linkstack
+    
